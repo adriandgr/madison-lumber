2647370ebf5946547072a7c46452c58016fa9509
Adrian Diaz <web.art@adriandiaz.ca>
2017-11-18 23:22:43 -0500

    Adds stories for AlertMessages component.

--- client/src/components/shared/AlertMessages.js
+++ client/src/components/shared/AlertMessages.js
@@ -7,61 +7,70 @@ const AlertMessages = props => {
   let infoCount = 0;
   let errCount = 0;
 
-  if((props.success.length > 0 || props.errors.length > 0 || (props.info && props.info.length > 0))  && props.scroll) {
+  if(((props.success && props.success.length > 0) ||
+      (props.errors && props.errors.length > 0) ||
+      (props.info && props.info.length > 0)) &&
+       props.scroll) {
     scroll.to('.alert-messages');
   }
-  return (
-  <div className="alert-messages">
-    { props.success.length > 0 &&
-      <div className="alert alert-success">
-        {props.success.map((msg, i) => {
-          msgCount++;
-          return (
-          <span key={`msg${msgCount}`}>
-            { i === 0
-              ? <strong>{msg} <br/></strong>
-              : <span>{msg} <br/></span> }
-          </span>
-          )
-        })}
-      </div>
-    }
+  if (props.success || props.errors || props.info) {
+    return (
+      <div className="alert-messages">
+        { props.success && props.success.length > 0 &&
+          <div className="alert alert-success">
+            {props.success.map((msg, i) => {
+              msgCount++;
+              return (
+              <span key={`msg${msgCount}`}>
+                { i === 0
+                  ? <strong>{msg} <br/></strong>
+                  : <span>{msg} <br/></span> }
+              </span>
+              )
+            })}
+          </div>
+        }
 
-    { props.info && props.info.length > 0 &&
-      <div className="alert alert-info">
-        {props.info.map((info, i) => {
-          infoCount++;
-          return (
-          <span key={`info${infoCount}`}>
-            { i === 0
-              ? <strong><i className="fa fa-info-circle" aria-hidden="true"></i> {info} <br/></strong>
-              : <span>{info} <br/></span> }
-          </span>
-          )
-        })}
-      </div>
-    }
+        { props.info && props.info.length > 0 &&
+          <div className="alert alert-info">
+            {props.info.map((info, i) => {
+              infoCount++;
+              return (
+              <span key={`info${infoCount}`}>
+                { i === 0
+                  ? <strong><i className="fa fa-info-circle" aria-hidden="true"></i> {info} <br/></strong>
+                  : <span>{info} <br/></span> }
+              </span>
+              )
+            })}
+          </div>
+        }
 
-    { props.errors.length > 0 &&
-      <div className="alert alert-danger">
-        {props.errors.map((err, i) => {
-          errCount++;
-          return (
-          <span key={`err${errCount}`}>
-            { i === 0
-              ? <strong>{err} <br/></strong>
-              : <span>{err} <br/></span> }
-          </span>
-          )
-        })}
+        { props.errors && props.errors.length > 0 &&
+          <div className="alert alert-danger">
+            {props.errors.map((err, i) => {
+              errCount++;
+              return (
+              <span key={`err${errCount}`}>
+                { i === 0
+                  ? <strong>{err} <br/></strong>
+                  : <span>{err} <br/></span> }
+              </span>
+              )
+            })}
+          </div>
+        }
       </div>
-    }
-  </div>
-)};
+    )
+  } else {
+    return null;
+  }
+};
 
 AlertMessages.propTypes = {
-  success: PropTypes.array.isRequired,
-  errors: PropTypes.array.isRequired
+  success: PropTypes.array,
+  errors: PropTypes.array,
+  info: PropTypes.array
 };
 
 export default AlertMessages;
