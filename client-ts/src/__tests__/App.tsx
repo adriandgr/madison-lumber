48056087c26be1d020c4add79381957fe0188c6c
Adrian Diaz <web.art@adriandiaz.ca>
2020-05-29 01:16:55 -0400

    fix circleci config

--- client-ts/src/__tests__/App.tsx
+++ client-ts/src/__tests__/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import App from '../components/App';
+import App from '../App';
 
 it('renders without crashing', () => {
   shallow(<App />);
