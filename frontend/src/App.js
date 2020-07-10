70c34d1aa53e8dafb9f3c507682cf523bdb9b68c
Adrian Diaz <web.art@adriandiaz.ca>
2020-07-10 17:09:29 -0400

    replace fontawesome with remixicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,53 @@
-import React, {useContext, useEffect} from 'react';
-import Cookies from 'universal-cookie'
-import {Switch, Route} from 'react-router-dom'
-import {UserContext} from "./contexts/userContext"
+import React, { useContext, useEffect } from "react";
+import Cookies from "universal-cookie";
+import { Switch, Route, useLocation } from "react-router-dom";
+import { UserContext } from "./contexts/userContext";
 
-import './App.scss';
+import "./App.scss";
+import "remixicon/fonts/remixicon.css"
 
-import NavBar from "./components/NavBar"
-import Footer from "./components/Footer"
-import Home from "./pages/Home"
-import Login from "./pages/Login/Login"
-import Mills from "./pages/Mills/Mills"
-import Mill from "./pages/Mill/Mill"
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import CookieNotice from "./components/CookieNotice";
+// import PrivacyPolicy from "./components/PrivacyPolicy"
 
-
+import Home from "./pages/Home";
+import Login from "./pages/Login/Login";
+import Mills from "./pages/Mills/Mills";
+import Mill from "./pages/Mill/Mill";
+import Users from "./pages/Users";
 
 function App() {
-  const {token,validateToken, logout} = useContext(UserContext)
+  const { token, validateToken, logout } = useContext(UserContext);
+  const location = useLocation();
 
-  useEffect(()=> {
-    const cookies = new Cookies()
-    const token = cookies.get('jwt')
+  useEffect(() => {
+    const cookies = new Cookies();
+    const token = cookies.get("jwt");
     if (token) {
-      validateToken(token)
+      validateToken(token);
     } else {
-      logout()
+      logout(location.pathname);
     }
-  }, [])
+  }, []);
 
   return (
     <div className="container__app_root">
-        <NavBar/>
-        <div className="content__wrap">
-          <Switch>
-            <Route exact path='/'>
-              <Home/>
-            </Route>
-            <Route path='/login'>
-              <Login/>
-            </Route>
-            <Route path='/mills'>
-              <Mills/>
-            </Route>
-            <Route path='/mill/:mill_uuid'>
-              <Mill/>
-            </Route>
-          </Switch>
-        </div>
-        <Footer/>
+      <CookieNotice />
+
+      <NavBar />
+      <div className="content__wrap">
+        <Switch>
+          <Route exact path="/"><Home /></Route>
+          <Route path="/login"><Login /></Route>
+          <Route exact path="/mills"><Mills /></Route>
+          <Route path="/mill/:mill_uuid"><Mill /></Route>
+          <Route exact path="/users"><Users /></Route>
+          <Route> <div>NOT FOUND</div></Route>
+        </Switch>
+      </div>
+
+      <Footer />
     </div>
   );
 }
